eb39bd75889b011b225a94115c0c900501affcd3
Blueowl91 <xjv552@alumni.ku.dk>
2019-11-06 00:54:30 +0100

    parser possibly done, need tests for it

--- code/configuraptor/src/ParserImpl.hs
+++ code/configuraptor/src/ParserImpl.hs
@@ -1,8 +1,208 @@
 module ParserImpl where
 
 import Absyn
--- other imports
+import Text.ParserCombinators.ReadP
+import Data.Char
 
 parseString :: String -> Either String IDB
-parseString = undefined
+parseString s =
+  case readP_to_S pDatabase s of
+    [] -> Left "couldn't parse input"
+    [(output, "")] -> Right output
+    [(_, remaining)] -> Left ("this part wasn't parsed: " ++ remaining)
 
+pDatabase :: ReadP IDB
+pDatabase = do
+  remover()
+  declz <- pDeclz ([], [])
+  eof
+  return declz
+
+pDeclz :: IDB -> ReadP IDB
+pDeclz (res, comp) = do
+    ressource <- pRessourceDecl
+    pDeclz (res ++ ressource, comp)
+  +++ do
+    component <- pComponentDecl
+    pDeclz (res, comp ++ [component])
+  <++
+    return (res, comp)
+
+pRessourceDecl :: ReadP [RName]
+pRessourceDecl = do
+  keyword "resource"
+  rNames <- pRNames []
+  symbol '.'
+  return rNames
+
+pRNames :: [RName] -> ReadP [RName]
+pRNames rnList = do
+    rName <- pRName
+    symbol ','
+    pRNames (rnList ++ [rName])
+  +++ do
+    rName <- pRName
+    return (rnList ++ [rName])
+
+pRName :: ReadP RName
+pRName = pName ""
+
+pName :: String -> ReadP String
+pName name = do
+    word <- pWord
+    symbolString '-'
+    pName (name ++ word ++ "-")
+  +++ do
+    word <- pWord
+    let final = name ++ word
+    if length (final) < 33
+      then remover final
+      else fail "name is too long"
+
+pWord :: ReadP String
+pWord = do
+  word <- munch1 (\x -> isLetter x || isDigit x)
+  let letters = filter isLetter word
+  if length letters /= 0
+    then return word
+    else fail "word doesn't contain any letters"
+
+pComponentDecl :: ReadP IComp
+pComponentDecl = do
+  keyword "component"
+  cName <- pCName
+  symbol ':'
+  clauses <- pClauses []
+  symbol '.'
+  return (IC cName clauses)
+
+pCName :: ReadP CName
+pCName = pName ""
+
+pClauses :: [Clause] -> ReadP [Clause]
+pClauses clauses = do
+    clause <- pClause
+    symbol ';'
+    pClauses (clauses ++ [clause])
+  +++ do
+    clause <- pClause
+    return (clauses ++ [clause])
+
+pClause :: ReadP Clause
+pClause = do
+    keyword "provides"
+    rSpec <- pRSpec
+    return (CKProvides, rSpec)
+  +++ do
+    keyword "uses"
+    rSpec <- pRSpec
+    return (CKUses, rSpec)
+  +++ do
+    keyword "requires"
+    rSpec <- pRSpec
+    return (CKRequires, rSpec)
+
+pRSpec :: ReadP RSpec
+pRSpec = do
+  term <- pTerm
+  pRSpec2 term
+
+pRSpec2 :: RSpec -> ReadP RSpec
+pRSpec2 rSpec = do
+    symbol '|'
+    term <- pTerm
+    rSpec3 <- pRSpec3 term
+    pRSpec2 (RSOr rSpec rSpec3)
+  <++
+    pRSpec3 rSpec
+
+pRSpec3 :: RSpec -> ReadP RSpec
+pRSpec3 rSpec = do
+    symbol ','
+    term <- pTerm
+    pRSpec2 (RSAnd rSpec term)
+  <++
+    return rSpec
+
+pTerm :: ReadP RSpec
+pTerm = do
+    name <- pRName
+    return (RSRes name)
+  +++ do
+    num <- pNum
+    rSpec <- pRSpec
+    return (RSNum num rSpec)
+  +++ do
+    symbol '('
+    rSpec <- pRSpec
+    symbol ')'
+    return rSpec
+
+pNum :: ReadP Int
+pNum = do
+  string <- munch1 isDigit
+  let num = read string
+  if num < 1000000
+    then do
+      check "a number"
+      return num
+    else fail ("number is to large")
+
+keyword :: String -> ReadP ()
+keyword s = do
+  mapM_ (\c -> satisfy (\x -> x == toLower c || x == toUpper c)) s
+  check s
+
+check :: String -> ReadP ()
+check s = do
+  l <- look
+  if null l
+  then return ()
+  else
+    let c = head l in
+      if not (isLetter c || c == '-' || isDigit c)
+      then do
+        remover ()
+      else fail ("expected: " ++ s)
+
+remover :: a -> ReadP a
+remover a = do
+    skipSpaces
+    comment
+    remover a
+  <++ do
+    skipSpaces
+    return a
+
+comment :: ReadP ()
+comment = do
+  symbolString '{'
+  munch (\x -> x /= '{' && x /= '}')
+  internalComments
+  symbolString '}'
+  return ()
+
+internalComments :: ReadP ()
+internalComments = do
+    comment
+    munch (\x -> x /= '{' && x /= '}')
+    internalComments
+    return ()
+  <++
+   return ()
+
+-- symbolString verifies that a given symbol is the next char we should parse
+-- is used inside strings
+symbolString :: Char -> ReadP ()
+symbolString c = do
+  c' <- satisfy (== c)
+  if c' == c then return ()
+    else fail ("expected: " ++ [c] ++ " but got: " ++ [c'])
+
+-- symbol does the same job as symbolString, but is not used inside strings,
+-- so therefore it removes whitespace
+symbol :: Char -> ReadP ()
+symbol c = do
+  c' <- satisfy (== c)
+  if c' == c then remover ()
+    else fail ("expected: " ++ [c] ++ " but got: " ++ [c'])
--- code/configuraptor/tests/Test.hs
+++ code/configuraptor/tests/Test.hs
@@ -9,7 +9,9 @@ import Test.Tasty
 import Test.Tasty.HUnit
 
 main :: IO ()
-main = defaultMain $ localOption (mkTimeout 1000000) tests
+main = defaultMain $ localOption (mkTimeout 1000000) all_tests
+
+all_tests = testGroup "all tests" [tests, my_tests]
 
 tests = testGroup "Minimal tests" [
   testCase "parser" $
@@ -25,3 +27,16 @@ tests = testGroup "Minimal tests" [
     dbf = ([R "r"], [("c", [(R "r", (1,0))])])
     goal = [(R "r", (0,1))]
     sol = [("c", 1)]
+
+
+my_tests = testGroup "my tests" [
+  testCase "one resource" $
+    parseString resource @?= Right resource_result,
+  testCase "components.cr" $
+    parseString components @?= Right components_result
+  ]
+  where
+    resource = "resource RGB."
+    resource_result = (["RGB"],[])
+    components =  "{Resources} \n resource GB-RAM, DIMM-slot. \n resource Intel-CPU-socket, AMD-CPU-socket, x86-processor. \n resource monitor, VGA-port, HDMI-port. \n resource OS. \n resource DKK. \n {RAM} \n component 8GB-module: \n   provides 8 GB-RAM; \n   uses 1 DIMM-slot; \n   uses 500 DKK. \n component 4GB-module: \n   provides 4 GB-RAM; \n   uses 1 DIMM-slot; \n   uses 300 DKK. \n {Motherboards} \n component Intel-motherboard: \n   provides 2 DIMM-slot; \n   provides Intel-CPU-socket; \n   provides VGA-port; {integrated graphics} \n   uses 500 DKK. \n component AMD-motherboard: \n   provides 2 DIMM-slot; \n   provides AMD-CPU-socket; \n   uses 600 DKK. \n {Processors} \n component Intel-i8: \n   provides x86-processor; \n   uses Intel-CPU-socket; \n   uses 800 DKK. \n component AMD-Ryzen6: \n   provides x86-processor; \n   uses AMD-CPU-socket; \n   uses 700 DKK. \n    \n {Graphics cards and monitors} \n component NVIDIA-GPU: \n   provides 2 HDMI-port; \n   uses 1500 DKK. \n component monitor1: \n   provides monitor; \n   requires HDMI-port; \n   uses 1200 DKK. \n component monitor2: \n   provides monitor; \n   requires HDMI-port | VGA-port; \n   uses 1500 DKK. \n {Software} \n component Linux: \n   provides OS; \n   requires x86-processor; \n   uses 0 DKK. {Free!} \n component Windows: \n   provides OS; \n   requires x86-processor; \n   uses 1000 DKK."
+    components_result = (["GB-RAM","DIMM-slot","Intel-CPU-socket","AMD-CPU-socket","x86-processor","monitor","VGA-port","HDMI-port","OS","DKK"],[IC "8GB-module" [(CKProvides,RSNum 8 (RSRes "GB-RAM")),(CKUses,RSNum 1 (RSRes "DIMM-slot")),(CKUses,RSNum 500 (RSRes "DKK"))],IC "4GB-module" [(CKProvides,RSNum 4 (RSRes "GB-RAM")),(CKUses,RSNum 1 (RSRes "DIMM-slot")),(CKUses,RSNum 300 (RSRes "DKK"))],IC "Intel-motherboard" [(CKProvides,RSNum 2 (RSRes "DIMM-slot")),(CKProvides,RSRes "Intel-CPU-socket"),(CKProvides,RSRes "VGA-port"),(CKUses,RSNum 500 (RSRes "DKK"))],IC "AMD-motherboard" [(CKProvides,RSNum 2 (RSRes "DIMM-slot")),(CKProvides,RSRes "AMD-CPU-socket"),(CKUses,RSNum 600 (RSRes "DKK"))],IC "Intel-i8" [(CKProvides,RSRes "x86-processor"),(CKUses,RSRes "Intel-CPU-socket"),(CKUses,RSNum 800 (RSRes "DKK"))],IC "AMD-Ryzen6" [(CKProvides,RSRes "x86-processor"),(CKUses,RSRes "AMD-CPU-socket"),(CKUses,RSNum 700 (RSRes "DKK"))],IC "NVIDIA-GPU" [(CKProvides,RSNum 2 (RSRes "HDMI-port")),(CKUses,RSNum 1500 (RSRes "DKK"))],IC "monitor1" [(CKProvides,RSRes "monitor"),(CKRequires,RSRes "HDMI-port"),(CKUses,RSNum 1200 (RSRes "DKK"))],IC "monitor2" [(CKProvides,RSRes "monitor"),(CKRequires,RSOr (RSRes "HDMI-port") (RSRes "VGA-port")),(CKUses,RSNum 1500 (RSRes "DKK"))],IC "Linux" [(CKProvides,RSRes "OS"),(CKRequires,RSRes "x86-processor"),(CKUses,RSNum 0 (RSRes "DKK"))],IC "Windows" [(CKProvides,RSRes "OS"),(CKRequires,RSRes "x86-processor"),(CKUses,RSNum 1000 (RSRes "DKK"))]])
